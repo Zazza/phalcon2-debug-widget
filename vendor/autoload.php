2b25d9e148aa2ea2475776d27aa252847ad780cf
Dmitry Samotoy <dmitry.samotoy@gmail.com>
2014-11-23 12:50:25 +0300

    composer path fix

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit4af18b37809904a401c16e5c4349f238::getLoader();
+return ComposerAutoloaderInit8eeceb644119884255e5085c12782ac4::getLoader();
